4dcc530feee0bca8dc8f0ba859cc0f077d32efc6
fitterramadan-tech <fitterramadan@gmail.com>
2025-11-19 16:58:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function hello() {
     alert("penasaran dengan kesehatanmu, Ayo!! cek kesehatan di healthy house");
 }
 function hello() {
-    alert("penasaran dengan kesehatanmu, Ayo!! cek kesehatan di healthy house");
+    alert("penasaran dengan kesehatanmu, Ayo!! cek kesehatan di healthy house 🥰🥰");
 }
 document.querySelector(".cek-nav").addEventListener("click", () => {
     alert("Menu Cek Kesehatan diklik.");
