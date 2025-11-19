dd08275edde6945045bf92228216c833f278a6f9
fitterramadan-tech <fitterramadan@gmail.com>
2025-11-19 13:45:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-function hello() {
-    alert("Halo! Script JS berjalan dengan benar.");
-}
-function hello() {
-    alert("Halo! Website modern siap digunakan.");
-}
-
 document.querySelector(".cek-nav").addEventListener("click", () => {
     alert("Menu Cek Kesehatan diklik.");
 });
