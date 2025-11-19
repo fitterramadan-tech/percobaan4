4408c3d32e4a3b4bd191c0ffee922eb539717c59
fitterramadan-tech <fitterramadan@gmail.com>
2025-11-19 12:19:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// Script kontak sederhana
-const form = document.getElementById("contact");
-const msgEl = document.getElementById("msg");
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  const name = document.getElementById("name").value.trim();
-
-  msgEl.textContent = `Terima kasih, ${name}! Pesan Anda telah diterima.`;
-  form.reset();
-});
+function hello() {
+    alert("Halo! Script JS berjalan dengan benar.");
+}
