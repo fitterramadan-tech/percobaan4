09bfa3c0b48988514cf2d7185377ab2a92c9eb49
fitterramadan-tech <fitterramadan@gmail.com>
2025-11-20 07:02:49 +0700

    Create tentang.js

---
+++ tentang.js
@@ -0,0 +1,16 @@
+// ===== Respon Mood Lucu =====
+document.getElementById("mood-happy").onclick = function () {
+    alert("Wah! Kamu spertinya sedang dalam kondisi perfectmu 😏✨ Jaga keseharianmu baik baik ya!");
+};
+
+document.getElementById("mood-normal").onclick = function () {
+    alert("Oke! jaga baik baik keseharianmu ya😐👍");
+};
+
+document.getElementById("mood-tired").onclick = function () {
+    alert("Lagi capek ya? 😴💤 Jangan lupa istirahat cukup!");
+};
+
+document.getElementById("mood-sick").onclick = function () {
+    alert("Aduh, mending kamu istirahat dulu atau minum obat 🥺");
+};
